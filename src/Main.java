081842dca1227d750f3c649864bbfd2225f95752
TabulaRasa <lforjust@gmail.com>
2023-10-22 00:22:45 +0300

    создал 2 книги и 2 автора

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author veniaminKaverin = new Author("Вениамин", "Каверин");
+        Book twoСaptains = new Book("Два капитана", veniaminKaverin, 1940);
+        Author arthurConanDoyle = new Author("Артур", "Конан Дойль");
+        Book theLostWorld = new Book("Затерянный мир", arthurConanDoyle, 1912);
+
     }
 }
