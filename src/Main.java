9eba75e0f551efafed6e280b8eb3dc0797025131
TabulaRasa <lforjust@gmail.com>
2023-10-22 00:26:55 +0300

    hw 12 is done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Author veniaminKaverin = new Author("Вениамин", "Каверин");
-        Book twoСaptains = new Book("Два капитана", veniaminKaverin, 1940);
+        Book twoCaptains = new Book("Два капитана", veniaminKaverin, 1939);
+
         Author arthurConanDoyle = new Author("Артур", "Конан Дойль");
         Book theLostWorld = new Book("Затерянный мир", arthurConanDoyle, 1912);
 
+        twoCaptains.setYearOfPublication(1640);
     }
 }
