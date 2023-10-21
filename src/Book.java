7756999321d09f9f0051533a59303086d88a2f3e
TabulaRasa <lforjust@gmail.com>
2023-10-22 00:03:31 +0300

    обавил сеттер в класс книг

--- src/Book.java
+++ src/Book.java
@@ -20,4 +20,8 @@ public class Book {
     public int getYearOfPublication() {
         return this.yearOfPublication;
     }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
 }
