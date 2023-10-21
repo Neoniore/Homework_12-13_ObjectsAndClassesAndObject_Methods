8d962b0ac8dbb60eb0dcbed97cfdc7c0bb756117
TabulaRasa <lforjust@gmail.com>
2023-10-21 23:22:56 +0300

    Создал поля в классе Author Создал конструкторы для обоих классов

--- src/Book.java
+++ src/Book.java
@@ -2,4 +2,10 @@ public class Book {
     private String title;
     private Author author;
     private int yearOfPublication;
+
+    public Book(String title, Author author, int yearOfPublication) {
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
 }
